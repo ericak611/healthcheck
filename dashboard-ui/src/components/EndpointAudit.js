6ac046010d70f003d20dd3665764ce887b28cd0b
Hyewon Kim <ericak611@gmail.com>
2023-11-13 22:12:36 -0800

    updated part 1

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -1,43 +1,47 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 
 export default function EndpointAudit(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [log, setLog] = useState(null);
-    const [error, setError] = useState(null)
+    const [error, setError] = useState(null);
     const [index, setIndex] = useState(null);
-    
-	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
-    setIndex(rand_val);
-    
-    const getAudit = () => {
-        fetch(`http://acit3855-system.eastus.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
-            .then(res => res.json())
-            .then((result)=>{
-				console.log("Received Audit Results for " + props.endpoint)
-                setLog(result);
-                setIsLoaded(true);
-            },(error) =>{
-                setError(error)
-                setIsLoaded(true);
-            })
-    }
-	useEffect(() => {
-		const interval = setInterval(() => getAudit(), 4000); // Update every 4 seconds
-		return() => clearInterval(interval);
-    }, [getAudit]);
 
-    if (error){
-        return (<div className={"error"}>Error found when fetching from API</div>)
-    } else if (isLoaded === false){
-        return(<div>Loading...</div>)
-    } else if (isLoaded === true){
-        
+    useEffect(() => {
+        const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+        setIndex(rand_val);
+
+        const getAudit = () => {
+            fetch(`http://acit3855-system.eastus.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
+                .then((res) => res.json())
+                .then(
+                    (result) => {
+                        console.log("Received Audit Results for " + props.endpoint);
+                        setLog(result);
+                        setIsLoaded(true);
+                    },
+                    (error) => {
+                        setError(error);
+                        setIsLoaded(true);
+                    }
+                );
+        };
+
+        const interval = setInterval(() => getAudit(), 4000); // Update every 4 seconds
+        return () => clearInterval(interval);
+    }, [props.endpoint]);
+
+    if (error) {
+        return <div className={"error"}>Error found when fetching from API</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
         return (
             <div>
                 <h3>{props.endpoint}-{index}</h3>
                 {JSON.stringify(log)}
             </div>
-        )
+        );
     }
 }
+
