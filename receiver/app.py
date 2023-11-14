e40a3247348a02406438176e2d9619aa25408d37
Hyewon Kim <ericak611@gmail.com>
2023-11-13 19:08:11 -0800

    part 5: Receiver Service Reconnects to Kafka for Each Event

--- receiver/app.py
+++ receiver/app.py
@@ -11,6 +11,7 @@ import random
 import uuid
 import pykafka 
 from pykafka import KafkaClient
+import time
 
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
@@ -26,6 +27,42 @@ logger = logging.getLogger('basicLogger')
 # topic = client.topics[str.encode(app_config['events']['topic'])]
 # producer = topic.get_sync_producer() 
 
+def connect_to_kafka(hostname, app_config):
+    retry_count = 0
+    max_retries = app_config["kafka"]["max_retries"]
+
+    while retry_count < max_retries:
+        try:
+            logger.info(f"Trying to connect to Kafka. Attempt #{retry_count + 1}")
+            client = KafkaClient(hosts=f"{hostname}")
+            topic = client.topics[str.encode(app_config["events"]["topic"])]
+            return client, topic
+        except Exception as e:
+            logger.error(f"Connection to Kafka failed. Error: {str(e)}")
+            sleep_time = app_config["kafka"]["sleep_seconds"]
+            logger.info(f"Retrying in {sleep_time} seconds...")
+            time.sleep(sleep_time)
+
+            retry_count += 1
+
+    logger.error(f"Failed to connect to Kafka after {max_retries} attempts.")
+    return None, None
+
+# Create KafkaClient when Receiver Service starts up
+client, topic = connect_to_kafka(
+    f"{app_config['events']['hostname']}:{app_config['events']['port']}",
+    app_config)
+
+def get_kafka_producer():
+    return topic.get_sync_producer()
+
+def reconnect_to_kafka():
+    global client, topic
+    logger.info("Reconnecting to Kafka...")
+    client, topic = connect_to_kafka(
+        f"{app_config['events']['hostname']}:{app_config['events']['port']}",
+        app_config)
+
 def add_book_hold(body): 
     trace_id = str(uuid.uuid4())
     message = f"Receievd event book hold request with trace_id: {trace_id}"
@@ -35,24 +72,28 @@ def add_book_hold(body):
         "trace_id": trace_id,
         **body
     }
-
-    # response = requests.post(
-    #     app_config['eventstore1']['url'],
-    #     json=body,
-    #     headers={"Content-Type": "application/json"}
-    # )
-    client = KafkaClient(hosts=f"{app_config['events']['hostname']}:{app_config['events']['port']}")
-    topic = client.topics[str.encode(app_config['events']['topic'])]
-    producer = topic.get_sync_producer() 
+    producer = get_kafka_producer()
     
-    msg = { "type": "book",
-            "datetime" :
-                datetime.datetime.now().strftime(
-                    "%Y-%m-%dT%H:%M:%S"),
-            "payload": event_object }
-    msg_str = json.dumps(msg)
-    producer.produce(msg_str.encode('utf-8'))
-    logger.info("Sending the book hold request to Kafka")
+    try:
+        msg = { "type": "book",
+                "datetime" :
+                    datetime.datetime.now().strftime(
+                        "%Y-%m-%dT%H:%M:%S"),
+                "payload": event_object }
+        msg_str = json.dumps(msg)
+        producer.produce(msg_str.encode('utf-8'))
+        logger.info("Sending the book hold request to Kafka")
+    except:
+        logger.error(f"Producer stopped. Attempting to reconnect to Kafka.")
+        reconnect_to_kafka()
+        producer = get_kafka_producer()
+
+        msg = {"type": "book",
+               "datetime": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+               "payload": event_object}
+        msg_str = json.dumps(msg)
+        producer.produce(msg_str.encode('utf-8'))
+        logger.info("Sent the book hold request to Kafka after reconnect")        
 
     # logger.info(f"Returned event movie hold request with response {trace_id} with status {response.status_code}")
     return NoContent, 201
@@ -68,25 +109,32 @@ def add_movie_hold(body):
         "trace_id": trace_id,
         **body
     }
+    producer = get_kafka_producer()
 
-    # response = requests.post(
-    #     app_config['eventstore2']['url'],
-    #     json=body,
-    #     headers={"Content-Type": "application/json"}
-    # )
-    client = KafkaClient(hosts=f"{app_config['events']['hostname']}:{app_config['events']['port']}")
-    topic = client.topics[str.encode(app_config['events']['topic'])]
-    producer = topic.get_sync_producer() 
-    
-    msg = { "type": "movie",
-            "datetime" :
-                datetime.datetime.now().strftime(
-                    "%Y-%m-%dT%H:%M:%S"),
-            "payload": event_object }
-    msg_str = json.dumps(msg)
-    producer.produce(msg_str.encode('utf-8'))
-    logger.info("Sending the book hold request to Kafka")
+    try:
+        producer = get_kafka_producer()
+        msg = { "type": "movie",
+                "datetime" :
+                    datetime.datetime.now().strftime(
+                        "%Y-%m-%dT%H:%M:%S"),
+                "payload": event_object }
+        msg_str = json.dumps(msg)
+        producer.produce(msg_str.encode('utf-8'))
+        logger.info("Sending the movie hold request to Kafka")
     # logger.info(f"Returned event movie hold request with response {trace_id} with status {response.status_code}")
+    except:
+        logger.error(f"Producer stopped. Attempting to reconnect to Kafka.")
+        reconnect_to_kafka()        
+        producer = get_kafka_producer()
+        
+        msg = { "type": "movie",
+                "datetime" :
+                    datetime.datetime.now().strftime(
+                        "%Y-%m-%dT%H:%M:%S"),
+                "payload": event_object }
+        msg_str = json.dumps(msg)
+        producer.produce(msg_str.encode('utf-8'))
+        logger.info("Sending the moive hold request to Kafka")        
     return NoContent, 201
 
 app = connexion.FlaskApp(__name__, specification_dir='')
